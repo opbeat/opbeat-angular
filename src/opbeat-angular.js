041537a7eff7ff5cff4f9adb98b16a9fdeb5d2c4
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-11-02 13:42:16 +0100

    feat(opbeat-angular): check for global serviceFactory

--- src/opbeat-angular.js
+++ src/opbeat-angular.js
@@ -1,9 +1,13 @@
 var opbeatCore = require('opbeat-js-core')
 var ServiceFactory = opbeatCore.ServiceFactory
 var angularInitializer = require('./angularInitializer')
+
 require('zone.js')
 function init () {
   var serviceFactory = new ServiceFactory()
+  if (window.opbeatApi && window.opbeatApi.serviceFactory) {
+    serviceFactory = window.opbeatApi.serviceFactory
+  }
   angularInitializer(serviceFactory)
 }
 
