05d35238d0033bee460da213bf182e2d3168ea51
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2017-02-17 15:04:37 +0100

    chore(fixtures): use setInitialPageLoadName

--- test/e2e/angular/no_router/no_router.js
+++ test/e2e/angular/no_router/no_router.js
@@ -1,20 +1,24 @@
 function init () {
   var app = angular.module('noRouter', ['ngOpbeat'])
   var ctrl = require('../common/exponential_ctrl')
+
+  var config = {
+    // logLevel: 'trace',
+    debug: true,
+    orgId: '7f9fa667d0a349dd8377bc740bcfc33e',
+    appId: '0a8757798e',
+    performance: {
+      enable: true,
+      enableStackFrames: true,
+      capturePageLoad: true,
+      captureInteractions: true
+    }
+  }
+
   app.config(function ($opbeatProvider) {
-    $opbeatProvider.config({
-      // logLevel: 'trace',
-      debug: true,
-      orgId: '7f9fa667d0a349dd8377bc740bcfc33e',
-      appId: '0a8757798e',
-      performance: {
-        enable: true,
-        enableStackFrames: true,
-        capturePageLoad: true,
-        captureInteractions: true
-      }
-    })
+    $opbeatProvider.config(config)
   })
+  opbeat.setInitialPageLoadName('hamid')
   app.controller('expCtrl', ctrl)
 }
 
